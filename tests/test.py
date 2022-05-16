e889696f7466948a333c488341dedf9760cee0a6
Barabazs <31799121+Barabazs@users.noreply.github.com>
2022-05-16 16:22:05 +0200

    test os.environ with actions

--- tests/test.py
+++ tests/test.py
@@ -4,15 +4,37 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-
-from utils import main
+from unittest.mock import patch
 import unittest
 
+from utils import main
+from utils import config
 
+
+
+@patch(
+    "utils.config.config",
+    {
+        "arbitrum_web3_provider": os.environ.get("ARBITRUM_ENDPOINT"),
+        "avalanche_web3_provider": os.environ.get("AVALANCHE_ENDPOINT"),
+        "binance_web3_provider": os.environ.get("BINANCE_ENDPOINT"),
+        "fantom_web3_provider": os.environ.get("FANTOM_ENDPOINT"),
+        "optimism_web3_provider": os.environ.get("OPTIMISM_ENDPOINT"),
+        "polygon_web3_provider": os.environ.get("POLYGON_ENDPOINT"),
+        "web3_provider": os.environ.get("WEB3_PROVIDER"),
+        "ipfs_gateway": os.environ.get("IPFS_GATEWAY"),
+        "opensea_api_key":os.environ.get("opensea_api_key"),
+        "moralis_api_key":os.environ.get("moralis_api_key")
+    },
+)
 class TestUtils(unittest.TestCase):
     def test_utils(self):
         self.assertIsInstance(main.hello(), str)
         self.assertEqual(main.hello(), "GM World!")
+
+    def test_config(self):
+        print(config.config)
+
 
 
 if __name__ == "__main__":  # pragma: no cover
