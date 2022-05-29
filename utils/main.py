2aa260b543784d29d9e33759d91ae4c890fcd21a
0xano <106316515+0xano@users.noreply.github.com>
2022-05-29 13:29:34 +0200

    Update main.py

--- utils/main.py
+++ utils/main.py
@@ -8,3 +8,16 @@ def hello():
 def use_config():
     print(config.ENDPOINT)
     return config.ENDPOINT
+
+
+def return_all_keys():
+    keys = {
+        "ENDPOINT": config.ENDPOINT,
+        "ARBITRUM_ENDPOINT": config.ARBITRUM_ENDPOINT,
+        "AVALANCHE_ENDPOINT": config.AVALANCHE_ENDPOINT,
+        "BINANCE_ENDPOINT": config.BINANCE_ENDPOINT,
+        "FANTOM_ENDPOINT": config.FANTOM_ENDPOINT,
+        "OPTIMISM_ENDPOINT": config.OPTIMISM_ENDPOINT,
+        "POLYGON_ENDPOINT": config.POLYGON_ENDPOINT,
+    }
+    return keys
